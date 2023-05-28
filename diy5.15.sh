fe9e910461534800963697773d760d6e5511310a
scalaview <chailink100@gmail.com>
2023-05-28 14:23:27 +0800

    diy some packages

--- diy5.15.sh
+++ diy5.15.sh
@@ -42,6 +42,7 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-ser
 git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 # git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/firkerword/luci-app-parentcontrol.git package/parentcontrol
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 # git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
